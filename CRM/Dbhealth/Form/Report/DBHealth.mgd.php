061276c9ca0f3744810ef821adaa058f60207e9c
Jamie McClelland <jm@mayfirst.org>
2013-12-03 13:00:44 -0500

    update meta data
    Removing Campaign as component so people can use this without
    enabling CiviCampaign

--- CRM/Dbhealth/Form/Report/DBHealth.mgd.php
+++ CRM/Dbhealth/Form/Report/DBHealth.mgd.php
@@ -15,7 +15,6 @@ return array (
       'description' => 'Database Health Report: Provides statistics on how extensively a Drupal-based CiviCRM database is being used',
       'class_name' => 'CRM_Dbhealth_Form_Report_DBHealth',
       'report_url' => 'net.ourpowerbase.report.dbhealth/dbhealth',
-      'component' => 'CiviCampaign',
     ),
   ),
 );
